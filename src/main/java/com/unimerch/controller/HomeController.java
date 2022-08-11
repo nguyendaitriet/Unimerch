096d32a9b13a13a1d50e4fa68b7ca07d54297f1a
Hân Nguyễn <hannguyen7796@gmail.com>
2022-08-11 10:22:24 +0700

    add login layout, add login function

--- src/main/java/com/unimerch/controller/HomeController.java
+++ src/main/java/com/unimerch/controller/HomeController.java
@@ -1,4 +1,20 @@
 package com.unimerch.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("")
 public class HomeController {
+    @GetMapping("/")
+    public String redirectToDashBoard() {
+        return "redirect:/dashboard";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView showLoginForm() {
+        return new ModelAndView("/login/login");
+    }
 }
