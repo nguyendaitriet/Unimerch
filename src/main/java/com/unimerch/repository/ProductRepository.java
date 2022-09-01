b03f3662eb62691f286baa30c4896dd0d2878dca
nguyendaitriet <triet9510@gmail.com>
2022-09-01 16:07:58 +0700

    get today sold product update

--- src/main/java/com/unimerch/repository/ProductRepository.java
+++ src/main/java/com/unimerch/repository/ProductRepository.java
@@ -1,9 +1,35 @@
 package com.unimerch.repository;
 
+import com.querydsl.core.Tuple;
+import com.unimerch.dto.product.ProductItemResult;
 import com.unimerch.repository.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+
+    @Query(
+        value = "SELECT  " +
+                    "SUM(o.purchased)-SUM(o.cancelled) AS quantitySold, " +
+                    "o.title AS productName, " +
+                    "SUM(o.royalties) AS royalties, " +
+                    "p.price, " +
+                    "o.amzn_account_id AS amznAccUsername, " +
+                    "o.ASIN AS asin " +
+                "FROM orders AS o " +
+                "INNER JOIN products AS p " +
+                "ON p.ASIN = o.ASIN " +
+                "WHERE o.amzn_account_id = :amznAccId " +
+                "AND o.date >= :today " +
+                "GROUP BY o.ASIN",
+        nativeQuery = true
+    )
+    List<Tuple> getProductItemResultList(@Param("today")Instant today, @Param("amznAccId") Integer amznAccId);
+
 }
--- src/main/java/com/unimerch/service/impl/ProductServiceImpl.java
+++ src/main/java/com/unimerch/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.unimerch.service.impl;
 
 import com.unimerch.dto.product.ProductItemResult;
+import com.unimerch.repository.ProductRepository;
 import com.unimerch.repository.datatable.ProductTableRepository;
 import com.unimerch.repository.model.Order;
 import com.unimerch.service.ProductService;
@@ -14,15 +15,34 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public List<ProductItemResult> findAllTodaySoldProduct(Integer amznAccId) {
+        Instant randomDate = Instant.parse("2022-07-02T00:00:00.000-07:00");
+
+//        List<ProductItemResult> productItemResultList =
+//                productRepository.getProductItemResultList(randomDate, amznAccId)
+//                        .stream()
+//                        .map(t -> new ProductItemResult(
+//                                t.get(0, Integer.class),
+//                                t.get(1, String.class),
+//                                t.get(2, BigDecimal.class),
+//                                t.get(3, BigDecimal.class),
+//                                t.get(4, Integer.class),
+//                                t.get(5, String.class)
+//                        ))
+//                        .collect(Collectors.toList());
         return null;
     }
-
 
 }
