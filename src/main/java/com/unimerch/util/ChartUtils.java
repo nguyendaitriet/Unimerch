e834de64acb4f562942445903ac5d030f9bb1492
Hân Nguyễn <hannguyen7796@gmail.com>
2022-09-21 08:12:27 +0700

    fix chart algorithm and user view

--- src/main/java/com/unimerch/util/ChartUtils.java
+++ src/main/java/com/unimerch/util/ChartUtils.java
@@ -5,39 +5,34 @@ import java.math.RoundingMode;
 
 public class ChartUtils {
     public static long getMaxAxis(BigDecimal num) {
-        int scale = -1;
+        if (num.compareTo(BigDecimal.ZERO) <= 0)
+            return num.add(BigDecimal.TEN).longValue();
 
+        int scale = -1;
         int numberDigits = getNumberDigits(num);
 
         if (numberDigits > 3)
             scale -= numberDigits - 3;
 
-        num = num.setScale(scale, RoundingMode.CEILING);
+        BigDecimal result = num.setScale(scale, RoundingMode.CEILING);
 
-        num = beautifyMaxAxis(num, numberDigits);
+        BigDecimal divisor = BigDecimal.valueOf(Math.pow(10, numberDigits - 2));
+        BigDecimal firstTwoDigits = result.divide(divisor);
 
-        return num.longValue();
+        if (result.compareTo(num) == 0) {
+            if (numberDigits > 3) {
+                firstTwoDigits = firstTwoDigits.add(BigDecimal.ONE);
+            } else {
+                firstTwoDigits = firstTwoDigits.add(BigDecimal.TEN);
+            }
+        }
+
+        result = firstTwoDigits.multiply(divisor);
+
+        return result.longValue();
     }
 
     public static int getNumberDigits(BigDecimal n) {
         return n.signum() == 0 ? 1 : n.precision() - n.scale();
-    }
-
-    public static BigDecimal beautifyMaxAxis(BigDecimal num, int numberDigits) {
-        if (num.compareTo(BigDecimal.ZERO) == 0) {
-            num = num.add(BigDecimal.TEN);
-            return num;
-        }
-
-        int ten = 10;
-        BigDecimal divisor = BigDecimal.valueOf(Math.pow(ten, numberDigits - 2));
-        BigDecimal firstTwoDigits = num.divide(divisor);
-
-        if (!firstTwoDigits.remainder(BigDecimal.valueOf(2)).equals(BigDecimal.ZERO))
-            firstTwoDigits = firstTwoDigits.add(BigDecimal.ONE);
-
-        num = firstTwoDigits.multiply(divisor);
-
-        return num;
     }
 }
