19f7718661ce0ad0913ea4ef88e73c3591469c2b
nguyendaitriet <triet9510@gmail.com>
2022-09-22 10:14:23 +0700

    refactor code

---
+++ src/main/java/com/unimerch/service/impl/ConfigurationServiceImpl.java
@@ -0,0 +1,13 @@
+package com.unimerch.service;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfigurationServiceImpl implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
