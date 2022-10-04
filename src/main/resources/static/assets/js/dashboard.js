a3ac0a8d286d560339459583aeaaab265bc550a6
Hân Nguyễn <hannguyen7796@gmail.com>
2022-10-04 10:35:01 +0700

    minor fix to redirect to login in handleFailedTasks()

--- src/main/resources/static/assets/js/dashboard.js
+++ src/main/resources/static/assets/js/dashboard.js
@@ -37,6 +37,7 @@ class DBApp {
                 CommonApp.IziToast.showErrorAlert(ERROR_400);
                 break;
             case 401:
+                window.alert = function () {};
                 CommonApp.SweetAlert.showTimeOut(
                     ERROR_401,
                     warningRedirect,
