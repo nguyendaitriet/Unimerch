546fe7ac65959031aea2b93864ca018f6efdbdc3
trancu900 <pet.cu20791@gmail.com>
2023-01-02 19:56:33 +0700

    Cu Add old api miss

--- src/main/java/com/unimerch/controller/api/OldAPI.java
+++ src/main/java/com/unimerch/controller/api/OldAPI.java
@@ -1,35 +1,41 @@
 package com.unimerch.controller.api;
 
 import com.unimerch.security.RoleConstant;
+import com.unimerch.service.OrderService;
 import com.unimerch.service.impl.ConfigurationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/apps")
-public class AppAPI {
+@RequestMapping("/api/amazone")
+public class OldAPI {
     @Autowired
-    private ConfigurationServiceImpl configurationService;
+    private OrderService orderService;
+
 
     @RoleConstant.AuthenticatedUser
-    @GetMapping("/getAppsConfig")
-    public ResponseEntity<?> getAppsConfigString() {
-        return new ResponseEntity<>(configurationService.getAppsConfigString(), HttpStatus.OK);
+    @GetMapping("/updateOrder")
+    public ResponseEntity<?> updateOrder() {
+
     }
 
     @RoleConstant.AuthenticatedUser
-    @PutMapping("/updateAppsConfig")
-    public ResponseEntity<?> updateAppsConfig(@RequestBody String newAppsConfig) {
-        return new ResponseEntity<>(configurationService.updateAppsConfig(newAppsConfig), HttpStatus.OK);
+    @PutMapping("/updateProduct")
+    public ResponseEntity<?> updateProduct(@RequestBody String newAppsConfig) {
+
     }
 
     @RoleConstant.ManagerAuthorization
-    @PutMapping("/updateBackendConfig")
-    public ResponseEntity<?> updateBackendConfig(@RequestBody String newBackendConfig) {
-        return new ResponseEntity<>(configurationService.updateBackendConfig(newBackendConfig), HttpStatus.OK);
+    @PutMapping("/updateAccountStatus")
+    public ResponseEntity<?> updateAccountStatus(@RequestBody String newBackendConfig) {
+
+    }
+    @RoleConstant.ManagerAuthorization
+    @PutMapping("/updateMetadata")
+    public ResponseEntity<?> updateAccountStatus(@RequestBody String newBackendConfig) {
+
     }
 
 
